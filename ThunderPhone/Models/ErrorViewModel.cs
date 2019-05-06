63b6963592ff222ec45084f16cd50882031e1cee
EliasCF <edimbo4@gmail.com>
2019-05-06 08:06:36 +0200

    Added project

---
+++ ThunderPhone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThunderPhone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
