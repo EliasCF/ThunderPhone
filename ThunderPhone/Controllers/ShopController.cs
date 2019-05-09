90dabd6529e822e112dfe4755217c4cbc02fde4f
EliasCF <edimbo4@gmail.com>
2019-05-09 14:07:37 +0200

    Made controller and views for the Shop part of the website

---
+++ ThunderPhone/Controllers/ShopController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThunderPhone.Controllers
+{
+    public class ShopController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Item()
+        {
+            return View();
+        }
+    }
+}
