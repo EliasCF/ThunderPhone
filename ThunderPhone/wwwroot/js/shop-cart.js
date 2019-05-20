d5938b1b1f315a0baaeb26493bd9ee7f3fd7cd51
EliasCF <edimbo4@gmail.com>
2019-05-20 13:17:53 +0200

    Each cart item now has an amount property

--- ThunderPhone/wwwroot/js/shop-cart.js
+++ ThunderPhone/wwwroot/js/shop-cart.js
@@ -2,5 +2,17 @@
     el: 'CartApp',
     data: {
         items: []
+    },
+    created: function () {
+        let cart = localStorage.getItem('cart');
+
+        //leave this.items empty if the cart item is empty or uncreated
+        if (cart === null || cart === '') {
+            return;
+        }
+
+        cart.split(',').forEach((item) => {
+            console.log(item);
+        });
     }
 });
--- ThunderPhone/wwwroot/js/shop-item.js
+++ ThunderPhone/wwwroot/js/shop-item.js
@@ -8,17 +8,38 @@ $('#AddToCart').on('click', () => {
         let cart = localStorage.getItem('cart');
 
         if (cart === null) {
-            localStorage.setItem('cart', [$('#ProductId').val()]);
+            localStorage.setItem('cart', JSON.stringify(
+                [{
+                    product: $('#ProductId').val(),
+                    amount: 1
+                }]));
 
             //Add one to the cart menu-link in the navigation bar
             $('#CartItems').html(parseInt($('#CartItems').html()) + 1);
             return;
         }
 
-        let cartArray = cart.split(' ');
-        cartArray.push($('#ProductId').val());
+        let cartArray = JSON.parse(cart);
+        let itemIsDuplicate = false;
 
-        localStorage.setItem('cart', cartArray);
+        //If the current item being added matches an items in the cart,
+        //then increment the amount property of that cart item
+        cartArray.forEach((item, index) => {
+            if (item.product === $('#ProductId').val()) {
+                cartArray[index].amount += 1;
+                itemIsDuplicate = true;
+            }
+        });
+
+        //Check if the current item was not found in the cart
+        if (!itemIsDuplicate) {
+            cartArray.push({
+                product: $('#ProductId').val(),
+                amount: 1
+            });
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cartArray));
 
         //Add one to the cart menu-link in the navigation bar
         $('#CartItems').html(parseInt($('#CartItems').html()) + 1);
