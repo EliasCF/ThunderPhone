bea43b19120c9e7af2aaf49d2ae63dde8bc0e86c
EliasCF <edimbo4@gmail.com>
2019-05-22 15:00:14 +0200

    The navbar now adjusts its product amount when amounts are adjusted on the cart page

--- ThunderPhone/wwwroot/js/shop-cart.js
+++ ThunderPhone/wwwroot/js/shop-cart.js
@@ -63,6 +63,12 @@
             //Find correct item and update amount property
             cart.forEach((item, index) => {
                 if (item.product === productId) {
+                    let amount = $('#CartItems').html() - cart[index].amount;
+                    amount += parseInt(newAmount);
+
+                    //Adjust product amount in navbar 
+                    $('#CartItems').html(parseInt(amount));
+
                     cart[index].amount = parseInt(newAmount);
                 }
             });
@@ -72,6 +78,10 @@
         removeItem: function (productId) {
             let cart = JSON.parse(localStorage.getItem('cart'));
 
+            //Remove the amount of the product from the overall amount displayed in the navbar
+            $('#CartItems').html(parseInt($('#CartItems').html()) - cart.filter(item => item.product === productId)[0].amount);
+
+            //Filter product out of localstorage item
             localStorage.setItem('cart',
                 JSON.stringify(
                     cart.filter(item =>
@@ -79,6 +89,7 @@
                 )
             );
 
+            //Remove product from items array
             this.items = this.items.filter(item => item.productId !== productId);
         }
     }
